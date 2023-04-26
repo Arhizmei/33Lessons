1ec1174577acdb06bceee0805d1e4733dbc8ae70
Arhizmei <arhizmei@gmail.com>
2023-04-26 20:41:18 +0300

    home2

--- app/src/main/java/com/zmei/a33lessons/MainActivity.kt
+++ app/src/main/java/com/zmei/a33lessons/MainActivity.kt
@@ -2,10 +2,21 @@ package com.zmei.a33lessons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zmei.a33lessons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.buttonFragment.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.placeHolder, BlankFragment.newInstance())
+                .commit()
+        }
+
     }
 }
